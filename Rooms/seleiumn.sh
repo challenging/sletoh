da7b3341553a699100eb2a6e1a711997a8f00aa1
Rungchi Chen <rungchi_chen@migocorp.com>
2014-10-28 09:48:53 +0800

    [Feature] - Integrate scrapy tasks to a shell
    Integrate the scrapy tasks with selenium to a shell script

---
+++ Rooms/seleiumn.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ws_path=$(dirname $0)
+
+run(){
+    local scrapy=$1
+    local which_hotel=$2
+
+    for city in $(ls ${which_hotel}/city*.cfg);
+    do
+        echo ${scrapy} ${city}
+    done
+}
+
+run ${ws_path}/Agoda/agoda.sh ${ws_path}/Agoda &
+run ${ws_path}/Rakuten/rakuten.sh ${ws_path}/Rakuten &
+
+wait
